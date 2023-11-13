ce4ba06d4ea631eaa9167a3c2773b2fcc98ef839
Alenteva <144173680+Alenteva@users.noreply.github.com>
2023-11-13 22:41:59 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -2,12 +2,6 @@ function getElementWidth(content, padding, border) {
   content = Number.parseFloat(content);
   padding = Number.parseFloat(padding);
   border = Number.parseFloat(border);
+
   return content + 2 * padding + 2 * border;
 }
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
